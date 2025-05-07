ba19379daea45c5e09954430825d596f2aa66b41
bit92 <sahabithun92@gmail.com>
2025-05-07 13:01:52 +0200

    final

---
+++ src/main/java/se/bithun/javawebdemo/models/POPSingerRepository.java
@@ -0,0 +1,8 @@
+package se.bithun.javawebdemo.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface POPSingerRepository extends JpaRepository<popSinger, Integer> {
+
+}
